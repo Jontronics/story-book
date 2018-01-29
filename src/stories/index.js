90de32db54429328fa6115c0d1306ed4c07d2759
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-01-29 09:53:51 -0500

    second story timer thing

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
 import React from 'react';
-
+import Timer from '../components/Timer';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+
 
 import Message from '../components/Message';
 
@@ -12,3 +13,5 @@ storiesOf('Message', module)
   .add('Hello World', () => <Message text="Hello world" />)
   .add("What's the time?", () => <Message text={new Date().toString()} />)
   .add('Custom', () => <Message text="Hello world" />)
+storiesOf('Timer', module)
+  .add('Show', () => <Timer />)
